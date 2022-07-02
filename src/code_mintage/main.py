b9cb6ddb871e91bcce714bec6551346a1b892fc3
Viacheslav <slava.k@ks2corp.com>
2022-07-02 22:25:27 +0300

    Added jlink builds

--- src/code_mintage/main.py
+++ src/code_mintage/main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
-from version import VERSION
+import argparse
 
+from colorama import Fore as Clr
+
+from code_mintage.jlink_command import (PATH_TO_EXE, build_jlink_command,
+                                        build_jlink_launcher_script)
+from code_mintage.version import VERSION
+
+DESCRIPTION = f'VSCode mintage {Clr.GREEN}v{VERSION}{Clr.RESET}'
 
 def main():
-    print(f"Version v{VERSION}")
+    print(DESCRIPTION)
+    print(build_jlink_command(device="STM32F407VE", path_to_exe=PATH_TO_EXE))
+    print(build_jlink_launcher_script())
 
 
 if __name__ == "__main__":
